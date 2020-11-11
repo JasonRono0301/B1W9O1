bb61b5f437dbfdf85c8d166b3231e08a4efaecf5
Jason R <99057964@mydavinci.nl>
2020-11-11 12:37:49 +0100

    Bijna klaar met de drank functie.

--- assets /horeca.js
+++ assets /horeca.js
@@ -3,16 +3,20 @@
 //Horeca App. 
 
 //Een variabele om de applicatie te starten.
-var startApp = horeca();
+var startDrank = horecaDrank();
 
 //Hieronder staan de dranken prijzen.
 const FRIS = 2.00;
 const BIER = 4.00;
 const WIJN = 5.00;
 
+//Hieronder staan de strings voor als het ongeldig is zodat het ik niet helemaal hoeft te typen.
+ "U heeft een ongeldige invoer gedaan";
+ "Uw bestelling kan niet worden toegevoegd";
 
-function horeca() {
-	//De gehele functie van de applicatie.
+
+function horecaDrank() {
+	//De gehele functie drank van de applicatie.
 	alert('Welkom bij de digitale horeca app');
 	alert('We beginnen eerst met de drank bestelling');
 	var drankbestelling = prompt('Type in "drank"');
@@ -20,15 +24,39 @@ function horeca() {
 		if (drankbestelling == "drank") {
 			drank();
 		} else{
-			alert('Type in: drank');
-			horeca();
+			alert("U heeft een ongeldige invoer gedaan");
+			horecaDrank();
 		}
 
 	function drank(){//Een functie voor de dranken.
 		alert('Welkom bij de drankbestelling');
 		alert('U kunt kiezen uit de categorie: fris of bier of wijn');
-		var drankkeuze = prompt('fris of bier of wijn?');
 
+		for(i = 0; i != "stop"; i++){//For lus voor de dranken bestelling.
+		  	var drankKeuze = prompt('Type in fris of bier of wijn?');
+				if(drankKeuze = "fris"){
+					var fris = prompt('Hoeveel frisdrank wilt u bestellen?');
+					var frisTotaal = FRIS * fris;
+
+				} else if(drankKeuze = "bier"){
+					var bier = prompt('Hoeveel bier wilt u bestellen?');
+					var bierTotaal = BIER * bier;
+
+				} else if (drankKeuze = "wijn") {
+					var wijn = prompt('Hoeveel wijn wilt u bestellen?');
+					var wijnTotaal = WIJN * wijn;
+
+				} else {
+					alert("Uw bestelling kan niet worden toegevoegd");
+				}
+
+				if (drankKeuze == "stop") {
+
+				} else {
+					alert( "U heeft een ongeldige invoer gedaan");
+				}
+		}
 		
+
 	}
 }
