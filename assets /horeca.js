774f1cf1aaaf09ce8e4e18cb374bfcab63dd3b5f
Jason R <99057964@mydavinci.nl>
2020-11-11 10:50:34 +0100

    Bezig om de gehele functie te bouwen.

--- assets /horeca.js
+++ assets /horeca.js
@@ -1,8 +1,34 @@
 //Jason Ronoastro
 //01-11-2020.
 //Horeca App. 
-function bestellingdrank() {
-    
-    
-}
 
+//Een variabele om de applicatie te starten.
+var startApp = horeca();
+
+//Hieronder staan de dranken prijzen.
+const FRIS = 2.00;
+const BIER = 4.00;
+const WIJN = 5.00;
+
+
+function horeca() {
+	//De gehele functie van de applicatie.
+	alert('Welkom bij de digitale horeca app');
+	alert('We beginnen eerst met de drank bestelling');
+	var drankbestelling = prompt('Type in "drank"');
+
+		if (drankbestelling == "drank") {
+			drank();
+		} else{
+			alert('Type in: drank');
+			horeca();
+		}
+
+	function drank(){//Een functie voor de dranken.
+		alert('Welkom bij de drankbestelling');
+		alert('U kunt kiezen uit de categorie: fris of bier of wijn');
+		var drankkeuze = prompt('fris of bier of wijn?');
+
+		
+	}
+}
