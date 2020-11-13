e6cf5ed70fc07fb5abf383d0e1fa685288149b2a
Jason R <99057964@mydavinci.nl>
2020-11-13 22:02:39 +0100

    Bezig met de drank functie

--- assets /horeca.js
+++ assets /horeca.js
@@ -10,13 +10,7 @@ const FRIS = 2.00;
 const BIER = 4.00;
 const WIJN = 5.00;
 
-//Hieronder staan de strings voor als het ongeldig is zodat het ik niet helemaal hoeft te typen.
- "U heeft een ongeldige invoer gedaan";
- "Uw bestelling kan niet worden toegevoegd";
-
-
-function horecaDrank() {
-	//De gehele functie drank van de applicatie.
+function horecaDrank() {//De gehele functie drank van de applicatie.
 	alert('Welkom bij de digitale horeca app');
 	alert('We beginnen eerst met de drank bestelling');
 	var drankbestelling = prompt('Type in "drank"');
@@ -28,35 +22,14 @@ function horecaDrank() {
 			horecaDrank();
 		}
 
-	function drank(){//Een functie voor de dranken.
+	function drank(){//Een functie om de dranken te bestellen.
 		alert('Welkom bij de drankbestelling');
-		alert('U kunt kiezen uit de categorie: fris of bier of wijn');
+		alert('Welk bestelling wilt u toevoegen?');
 
-		for(i = 0; i != "stop"; i++){//For lus voor de dranken bestelling.
-		  	var drankKeuze = prompt('Type in fris of bier of wijn?');
-				if(drankKeuze = "fris"){
-					var fris = prompt('Hoeveel frisdrank wilt u bestellen?');
-					var frisTotaal = FRIS * fris;
-
-				} else if(drankKeuze = "bier"){
-					var bier = prompt('Hoeveel bier wilt u bestellen?');
-					var bierTotaal = BIER * bier;
-
-				} else if (drankKeuze = "wijn") {
-					var wijn = prompt('Hoeveel wijn wilt u bestellen?');
-					var wijnTotaal = WIJN * wijn;
-
-				} else {
-					alert("Uw bestelling kan niet worden toegevoegd");
-				}
-
-				if (drankKeuze == "stop") {
-
-				} else {
-					alert( "U heeft een ongeldige invoer gedaan");
-				}
+		for(drankVraag = 0; drankVraag != "stop"; drankVraag++){
+			drankVraag = prompt('fris of bier of wijn?');
 		}
-		
+
 
 	}
 }
