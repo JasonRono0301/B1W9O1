253e8aa34bb5bf361fb9bf3dc16f40629b3c7ae2
Jason R <99057964@mydavinci.nl>
2020-12-09 19:39:11 +0100

    Bezig om de drank functie af te maken.

--- assets /horeca.js
+++ assets /horeca.js
@@ -16,14 +16,31 @@ function horecaDrank() {//De gehele functie drank van de applicatie.
 	alert('Welkom bij de digitale horeca app');
 	alert('We beginnen eerst met de drank bestelling');
 	var drankbestelling = prompt('Type in "drank"');
+	if(drankbestelling == "drank"){
+		alert('U word doorgestuurd om uw dranken te bestellen.');
+		drank();//Zodra de prompt string goed is ingevuld dan word je doorgestuurd naar de functie drank.
+	} else{
+		alert('Type drank in.');
+		horecaDrank();
+	}
 
-		if (drankbestelling == "drank") {
-			drankKeuze();
-		} else{
-			alert("U heeft een ongeldige invoer gedaan");
+}
+
+function drank() {
+	alert('Welkom bij de dranken bestelling');
+	var dranken;
+	dranken = prompt('Welke drank wilt u bestellen? fris,bier,wijn. Type "stop" als je de bon wilt krijgen.');
+
+		if(dranken == "fris"){
+			frisAantal = prompt('Hoeveel aantal frisdranken wilt u bestellen?');
+		} else if(dranken == "bier"){
+			bierAantal = prompt('Hoeveel aantal bier dranken wilt u bestellen?');
+		} else if(dranken == "wijn"){
+			wijnAantal = prompt('Hoeveel aantal wijn dranken wilt u bestellen?');
 		}
 
 }
 
-//De functie word aangeroepen.
+//De functies word aangeroepen.
 horecaDrank();
+drank();
