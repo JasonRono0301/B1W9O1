9f186e18a183b79e9b25998abeff99df63468b3f
Jason R <99057964@mydavinci.nl>
2020-12-11 11:38:35 +0100

    Heb de dranken bestelling af.

--- assets /horeca.js
+++ assets /horeca.js
@@ -12,9 +12,40 @@ const FRIS = 2.00;
 const BIER = 4.00;
 const WIJN = 5.00;
 
-function startOrder(){
-	alert('Welkom bij de Digital order app.')
+//Intro voor de app.
+alert('Welkom bij de Digital order app.');
+alert('We starten met de dranken bestelling.');
+
+function drinkOrder(){
+	while (DrinkChoice = true){
+		var DrinkChoice = prompt('Welke bestelling wilt u toevoegen?. fris,bier of wijn. Type stop om naar de rekening te gaan.');
+		if(DrinkChoice == "fris"){
+			frisAantal = prompt('Hoeveel fris wilt u bestellen?');
+			console.log(frisAantal);
+		} else if(DrinkChoice == "bier"){
+			bierAantal = prompt('Hoeveel bier wilt u bestellen?');
+		} else if(DrinkChoice == "wijn"){
+			wijnAantal = prompt('Hoeveel wijn wilt u bestellen?');
+		} else if(DrinkChoice = "stop"){
+			DrinkChoice = false;
+			alert('Uw rekening krijgt u nu te zien');
+			document.write('Uw aantal bestelde frisdranken = ' + frisAantal + "<br>");
+			document.write('Uw aantal bestelde bier dranken = ' + bierAantal + "<br>");
+			document.write('Uw aantal bestelde wijn dranken = ' + wijnAantal + "<br>");
+			var DrinksTotal = frisAantal*FRIS + bierAantal*BIER + wijnAantal*WIJN;
+			document.write('De totale prijs = ' + DrinksTotal +' '+ 'Euro');
+			return;
+		}  else{
+			alert('U heeft een ongeldige invoer gedaan.');
+		}
+	}
+
+
+
+
+
 }
 
-
 //De functies worden hier aangeroepen.
+drinkOrder();
+
