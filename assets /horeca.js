8ebefd710c34596130b907b45ae0ce9ecc332e06
Jason R <99057964@mydavinci.nl>
2020-12-11 10:44:28 +0100

    Opnieuw aan het maken.
    Ben de eind opdracht opnieuw aan het maken.

--- assets /horeca.js
+++ assets /horeca.js
@@ -12,35 +12,9 @@ const FRIS = 2.00;
 const BIER = 4.00;
 const WIJN = 5.00;
 
-function horecaDrank() {//De gehele functie drank van de applicatie.
-	alert('Welkom bij de digitale horeca app');
-	alert('We beginnen eerst met de drank bestelling');
-	var drankbestelling = prompt('Type in "drank"');
-	if(drankbestelling == "drank"){
-		alert('U word doorgestuurd om uw dranken te bestellen.');
-		drank();//Zodra de prompt string goed is ingevuld dan word je doorgestuurd naar de functie drank.
-	} else{
-		alert('Type drank in.');
-		horecaDrank();
-	}
-
+function startOrder(){
+	alert('Welkom bij de Digital order app.')
 }
 
-function drank() {
-	alert('Welkom bij de dranken bestelling');
-	var dranken;
-	dranken = prompt('Welke drank wilt u bestellen? fris,bier,wijn. Type "stop" als je de bon wilt krijgen.');
 
-		if(dranken == "fris"){
-			frisAantal = prompt('Hoeveel aantal frisdranken wilt u bestellen?');
-		} else if(dranken == "bier"){
-			bierAantal = prompt('Hoeveel aantal bier dranken wilt u bestellen?');
-		} else if(dranken == "wijn"){
-			wijnAantal = prompt('Hoeveel aantal wijn dranken wilt u bestellen?');
-		}
-
-}
-
-//De functies word aangeroepen.
-horecaDrank();
-drank();
+//De functies worden hier aangeroepen.
